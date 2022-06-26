42dc5796af259fee11871395092f3c9615d19e26
griby51 <77160694+griby51@users.noreply.github.com>
2022-06-26 17:00:29 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,238 @@
+statusbars.onStatusReached(StatusBarKind.Health, statusbars.StatusComparison.LTE, statusbars.ComparisonType.Percentage, 0, function (status) {
+    game.over(false)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 7 . . . . . . . . . . 7 . . 
+        . . 7 . . . . . . . . . . 7 . . 
+        . . 7 . . . . . . . . . . 7 . . 
+        . . 7 . . . . . . . . . . 7 . . 
+        . . 7 . . . . . . . . . . 7 . . 
+        . . 7 . . . . . . . . . . 7 . . 
+        . . 7 . . . . . . . . . . 7 . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, -100)
+    music.pewPew.play()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.warmRadial, 500)
+    sprite.destroy()
+    music.smallCrash.play()
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    statusbar.value += -1
+    music.bigCrash.play()
+})
+let projectile2: Sprite = null
+let projectile: Sprite = null
+let statusbar: StatusBarSprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`My sprite`, SpriteKind.Player)
+scene.setBackgroundImage(assets.image`myImage0`)
+mySprite.setPosition(76, 100)
+let projectile_texture = [
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c c . . . . . . 
+    . . . . . . a b a a . . . . . . 
+    . . . . . c b a f c a c . . . . 
+    . . . . c b b b f f a c c . . . 
+    . . . . b b f a b b a a c . . . 
+    . . . . c b f f b a f c a . . . 
+    . . . . . c a a c b b a . . . . 
+    . . . . . . c c c c . . . . . . 
+    . . . . . . . c . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c . . . . . . 
+    . . . . . c a a a a . . . . . . 
+    . . . . . a a f f b a . . . . . 
+    . . . . c a b f f c b . . . . . 
+    . . . . c b b b a f c b . . . . 
+    . . . . c b a c a b b b . . . . 
+    . . . . . b b f f a a c . . . . 
+    . . . . . . a a b b c . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c . . . . . . . 
+    . . . . c a a a a . . . . . . . 
+    . . . . a a f f b a . . . . . . 
+    . . . c a b f f c b . . . . . . 
+    . . . c b b b a f c b . . . . . 
+    . . . c b a c a b b b . . . . . 
+    . . . . b b f f a a c . . . . . 
+    . . . . . a a b b c . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . c c 8 . . . . 
+    . . . . . . 8 c c c f 8 c c . . 
+    . . . c c 8 8 f c a f f f c c . 
+    . . c c c f f f c a a f f c c c 
+    8 c c c f f f f c c a a c 8 c c 
+    c c c b f f f 8 a c c a a a c c 
+    c a a b b 8 a b c c c c c c c c 
+    a f c a a b b a c c c c c f f c 
+    a 8 f c a a c c a c a c f f f c 
+    c a 8 a a c c c c a a f f f 8 a 
+    . a c a a c f f a a b 8 f f c a 
+    . . c c b a f f f a b b c c 6 c 
+    . . . c b b a f f 6 6 a b 6 c . 
+    . . . c c b b b 6 6 a c c c c . 
+    . . . . c c a b b c c c . . . . 
+    . . . . . c c c c c c . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . c c c c . . 
+    . c c c c c . c c c c c f c c . 
+    c c a c c c c c 8 f f c f f c c 
+    c a f a a c c a f f c a a f f c 
+    c a 8 f a a c a c c c a a a a c 
+    c b c f a a a a a c c c c c c c 
+    c b b a a c f 8 a c c c 8 c c c 
+    . c b b a b c f a a a 8 8 c c . 
+    . . . . a a b b b a a 8 a c . . 
+    . . . . c b c a a c c b . . . . 
+    . . . . b b c c a b b a . . . . 
+    . . . . b b a b a 6 a . . . . . 
+    . . . . c b b b 6 6 c . . . . . 
+    . . . . . c a 6 6 b c . . . . . 
+    . . . . . . . c c c . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . c b a c . . . . . . 
+    . . . . c c b c f a c . . . . . 
+    . . . . a f b b b a c . . . . . 
+    . . . . a f f b a f c c . . . . 
+    . . . . c b b a f f c . . . . . 
+    . . . . . b b a f a . . . . . . 
+    . . . . . . c b b . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . c c . . . . . . . . 
+    . . . . c a f b c . . . . . . . 
+    . . . . b f f b c c . . . . . . 
+    . . . a a f b a b a c . . . . . 
+    . . . c a c b b f f b . . . . . 
+    . . . . b f f b f a b . . . . . 
+    . . . . a f f b b b a . . . . . 
+    . . . . . a b b c c . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . c c c c . . . . 
+    . . . . c c c c c c c c c . . . 
+    . . . c f c c a a a a c a c . . 
+    . . c c f f f f a a a c a a c . 
+    . . c c a f f c a a f f f a a c 
+    . . c c a a a a b c f f f a a c 
+    . c c c c a c c b a f c a a c c 
+    c a f f c c c a b b 6 b b b c c 
+    c a f f f f c c c 6 b b b a a c 
+    c a a c f f c a 6 6 b b b a a c 
+    c c b a a a a b 6 b b a b b a . 
+    . c c b b b b b b b a c c b a . 
+    . . c c c b c c c b a a b c . . 
+    . . . . c b a c c b b b c . . . 
+    . . . . c b b a a 6 b c . . . . 
+    . . . . . . b 6 6 c c . . . . . 
+    `,
+img`
+    . . . . . . . c c c a c . . . . 
+    . . c c b b b a c a a a c . . . 
+    . c c a b a c b a a a b c c . . 
+    . c a b c f f f b a b b b a . . 
+    . c a c f f f 8 a b b b b b a . 
+    . c a 8 f f 8 c a b b b b b a . 
+    c c c a c c c c a b c f a b c c 
+    c c a a a c c c a c f f c b b a 
+    c c a b 6 a c c a f f c c b b a 
+    c a b c 8 6 c c a a a b b c b c 
+    c a c f f a c c a f a c c c b . 
+    c a 8 f c c b a f f c b c c c . 
+    . c b c c c c b f c a b b a c . 
+    . . a b b b b b b b b b b b c . 
+    . . . c c c c b b b b b c c . . 
+    . . . . . . . . c b b c . . . . 
+    `,
+img`
+    . . . . . . c c c . . . . . . . 
+    . . . . . a a a c c c . . . . . 
+    . . . c a c f a a a a c . . . . 
+    . . c a c f f f a f f a c . . . 
+    . c c a c c f a a c f f a c . . 
+    . a b a a c 6 a a c c f a c c c 
+    . a b b b 6 a b b a a c a f f c 
+    . . a b b a f f b b a a c f f c 
+    c . a a a c c f c b a a c f a c 
+    c c a a a c c a a a b b a c a c 
+    a c a b b a a 6 a b b 6 b b c . 
+    b a c b b b 6 b c . c c a c . . 
+    b a c c a b b a c . . . . . . . 
+    b b a c a b a a . . . . . . . . 
+    a b 6 b b a c . . . . . . . . . 
+    . a a b c . . . . . . . . . . . 
+    `
+]
+statusbar = statusbars.create(70, 4, StatusBarKind.Health)
+statusbar.setColor(7, 2)
+statusbar.max = 3
+statusbar.setPosition(46, 11)
+forever(function () {
+    controller.moveSprite(mySprite, 100, 0)
+})
+forever(function () {
+    projectile2 = sprites.createProjectileFromSide(projectile_texture._pickRandom(), 0, 100)
+    projectile2.setPosition(randint(160, 0), -3)
+    projectile2.setKind(SpriteKind.Enemy)
+    projectile2.startEffect(effects.fire)
+    pause(500)
+})
